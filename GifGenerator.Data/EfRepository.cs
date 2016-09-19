605898867b375f5cc459ae050c4930276cd68fa5
MehrdadKamelzadeh <m.kamelzadeh@golestanec.com>
2016-09-19 19:50:47 +0430

    Repository Layer Added

---
+++ GifGenerator.Data/EfRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace GifGenerator.Data
+{
+    public class EfRepository<T> : IRepository<T> where T : IEntity
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public EfRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _context.SaveChanges();
+        }
+
+        public virtual T FindById(long id)
+        {
+            return _dbSet.Single(o => o.Id == id);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+    }
+}
